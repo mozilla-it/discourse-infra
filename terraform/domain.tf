2e6ac3d5615a33893a503aa93157e67fed63a9d7
Alberto del Barrio <The-smooth-operator@users.noreply.github.com>
2019-08-07 15:54:25 +0200

    add ACM SSL cert (#5)

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -17,3 +17,27 @@ data "aws_route53_zone" "common" {
 data "aws_elb" "k8s-elb" {
  name = "ab30fe62db90e11e99aba06db27de6a9"
 }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${aws_route53_record.discourse.name}"
+  validation_method = "DNS"
+  tags = "${merge(var.common-tags, var.workspace-tags)}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.common.id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+}
+
