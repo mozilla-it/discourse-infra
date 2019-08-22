06e7394052944e646e064749c8b2a936cc42f068
Alberto del Barrio <The-smooth-operator@users.noreply.github.com>
2019-08-22 09:35:36 +0200

    Create discourse devs user and group (#16)
    * Document user creation and cluster auth process
    
    * Create users and groups for discourse developers

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # discourse-infra
 This repository contains the infrastructure code for running discourse.mozilla.org
+
+# User management
+This section describes how to create new users in AWS and how can they connect to the Kubernetes cluster.
+In order connect to the cluster, a user must have installed the next dependencies:
+ - awscli >= 1.16.154
+ - kubectl >= 1.13 
+
+## Creating a user in AWS
+AWS users are managed using Terraform. In order to create a new user, one has to edit the file `terraform/users.tf`. Copy the resources used by the user `lmcardle`, using the name of the new user and `terraform apply the changes`.
+
+Once Terraform finishes, it should have created a set of credentials which will allow the user to visit the AWS Console. These credentials can be obtained running `terraform output password_username`, the output is a base64 blob containing the GPG encoded password. Now, give the credentials to the new user, she can decrypt the password running: `cat terraform_ouput_pw | base64 -d | keybase gpg decrypt`.
+
+### Enrolling an MFA device
+The default user policy enforces a user to setup MFA, until it is enabled the user will not be able to see or interact without any AWS resource.
+In order to enable MFA for her account, the new user has to login into the AWS console, go to the IAM secction and follow the steps described in the UI to enroll her MFA device
+
+### Generate API keys
+Now the user can generate a pair of API keys for using the command line interface. Inside the AWS console, go to IAM, select your user, click on "Security Credentials" and generate them.
+
+## Grant access to the Kubernetes cluster
+Granting access for a user to the Kubernetes cluster requires having 2 things: an AWS user with permissions to describe the target EKS cluster (this was done in the previous step), and a role inside Kubernetes granting permissions to certain namespaces. This role is already created for discourse developers, granting access to the 3 namespaces containing the different environments.
+
+Now the AWS user has to be mapped to the Kubernetes role. In order to perform the mapping, edit the 'aws-auth' configmap and reflect the new mapping: `kubectl edit configmap aws-auth -n=kube-system`.
+
+## Get EKS credentials and test access
+The configuration file which instructs kubectl how to connect to the cluster can be hand crafted, of obtained using the AWS cli. Obtaining it via the AWS cli is the easiest option and can be done running: `aws eks update-kubeconfig --name k8s-apps-prod-us-west-2`, this will create a file in `~/.kube/config'.
+
+It should be all set. Test the access running `kubectl get pods -n=discourse-dev`. If you see a list of pods, you are ready to go. 
---
+++ terraform/users.tf
@@ -0,0 +1,282 @@
+####################
+#      Users       #
+####################
+
+# Alberto
+resource "aws_iam_user" "adelbarrio" {
+  name = "adelbarrio"
+
+  # Not supported by EKS:
+  #path = "/discourse/"
+  tags = "${merge(var.common-tags, var.workspace-tags)}"
+}
+
+resource "aws_iam_user_policy" "adelbarrio_mfa" {
+  name = "allow-${aws_iam_user.adelbarrio.name}-self-manage-mfa"
+  user = "${aws_iam_user.adelbarrio.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowIndividualUserToListOnlyTheirOwnMFA",
+      "Effect": "Allow",
+      "Action": "iam:ListMFADevices",
+      "Resource": [
+        "arn:aws:iam::*:mfa/*",
+        "arn:aws:iam::*:user/${aws_iam_user.adelbarrio.name}"
+      ]
+    },
+    {
+      "Sid": "AllowIndividualUserToManageTheirOwnMFA",
+      "Effect": "Allow",
+      "Action": [
+        "iam:CreateVirtualMFADevice",
+        "iam:DeleteVirtualMFADevice",
+        "iam:EnableMFADevice",
+        "iam:ResyncMFADevice"
+      ],
+      "Resource": [
+        "arn:aws:iam::*:mfa/${aws_iam_user.adelbarrio.name}",
+        "arn:aws:iam::*:user/${aws_iam_user.adelbarrio.name}"
+      ]
+    },
+    {
+      "Sid": "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA",
+      "Effect": "Allow",
+      "Action": "iam:DeactivateMFADevice",
+      "Resource": [
+        "arn:aws:iam::*:mfa/${aws_iam_user.adelbarrio.name}",
+        "arn:aws:iam::*:user/${aws_iam_user.adelbarrio.name}"
+      ],
+      "Condition": {
+         "Bool": {
+           "aws:MultiFactorAuthPresent": "true"
+          }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+				"iam:ChangePassword",
+        "iam:CreateAccessKey",
+        "iam:DeleteAccessKey",
+        "iam:GetAccessKeyLastUsed",
+        "iam:GetUser",
+        "iam:ListAccessKeys",
+        "iam:UpdateAccessKey"
+			],
+      "Resource": "arn:aws:iam::783633885093:user/${aws_iam_user.adelbarrio.id}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_login_profile" "adelbarrio" {
+  user                    = "${aws_iam_user.adelbarrio.name}"
+  pgp_key                 = "keybase:adelbarrio"
+  password_reset_required = false
+}
+
+resource "aws_iam_group_membership" "adelbarrio" {
+  name  = "discourse-developer"
+  users = ["${aws_iam_user.adelbarrio.name}"]
+  group = "${aws_iam_group.developers.name}"
+}
+
+output "password_adelbarrio" {
+  value = "${aws_iam_user_login_profile.adelbarrio.encrypted_password}"
+}
+
+# Leo
+resource "aws_iam_user" "lmcardle" {
+  name = "lmcardle"
+
+  # Not supported by EKS:
+  #path = "/discourse/"
+  tags = "${merge(var.common-tags, var.workspace-tags)}"
+}
+
+resource "aws_iam_user_policy" "lmcardle_mfa" {
+  name = "allow-${aws_iam_user.lmcardle.name}-self-manage-mfa"
+  user = "${aws_iam_user.lmcardle.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowIndividualUserToListOnlyTheirOwnMFA",
+      "Effect": "Allow",
+      "Action": "iam:ListMFADevices",
+      "Resource": [
+        "arn:aws:iam::*:mfa/*",
+        "arn:aws:iam::*:user/${aws_iam_user.lmcardle.name}"
+      ]
+    },
+    {
+      "Sid": "AllowIndividualUserToManageTheirOwnMFA",
+      "Effect": "Allow",
+      "Action": [
+        "iam:CreateVirtualMFADevice",
+        "iam:DeleteVirtualMFADevice",
+        "iam:EnableMFADevice",
+        "iam:ResyncMFADevice"
+      ],
+      "Resource": [
+        "arn:aws:iam::*:mfa/${aws_iam_user.lmcardle.name}",
+        "arn:aws:iam::*:user/${aws_iam_user.lmcardle.name}"
+      ]
+    },
+    {
+      "Sid": "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA",
+      "Effect": "Allow",
+      "Action": "iam:DeactivateMFADevice",
+      "Resource": [
+        "arn:aws:iam::*:mfa/${aws_iam_user.lmcardle.name}",
+        "arn:aws:iam::*:user/${aws_iam_user.lmcardle.name}"
+      ],
+      "Condition": {
+         "Bool": {
+           "aws:MultiFactorAuthPresent": "true"
+          }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+				"iam:ChangePassword",
+        "iam:CreateAccessKey",
+        "iam:DeleteAccessKey",
+        "iam:GetAccessKeyLastUsed",
+        "iam:GetUser",
+        "iam:ListAccessKeys",
+        "iam:UpdateAccessKey"
+			],
+      "Resource": "arn:aws:iam::783633885093:user/${aws_iam_user.lmcardle.id}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_login_profile" "lmcardle" {
+  user                    = "${aws_iam_user.lmcardle.name}"
+  pgp_key                 = "keybase:leomca"
+  password_reset_required = false
+}
+
+resource "aws_iam_group_membership" "lmcardle" {
+  name  = "discourse-developer"
+  users = ["${aws_iam_user.lmcardle.name}"]
+  group = "${aws_iam_group.developers.name}"
+}
+
+output "password_lmcardle" {
+  value = "${aws_iam_user_login_profile.lmcardle.encrypted_password}"
+}
+
+#####################
+#   Group policies  #
+#####################
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+  path = "/discourse/"
+}
+
+resource "aws_iam_group_policy" "discourse-devs" {
+  name  = "discourse-developer-policy"
+  group = "${aws_iam_group.developers.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "eks:DescribeCluster",
+        "eks:ListClusters",
+				"codebuild:ListProjects"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "iam:GetAccountPasswordPolicy",
+       "Resource": "*"
+    },
+    {
+      "Action": [
+        "codebuild:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_codebuild_project.discourse.arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "events:DescribeRule",
+        "events:ListTargetsByRule",
+        "events:ListRuleNamesByTarget",
+				"cloudwatch:GetMetricStatistics",
+        "s3:GetBucketLocation",
+        "s3:ListAllMyBuckets",
+        "logs:GetLogEvents"
+			],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:PutParameter"
+      ],
+      "Resource": "arn:aws:ssm:*:*:parameter/CodeBuild/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy" "self-managed-mfa" {
+  name  = "self-managed-mfa"
+  group = "${aws_iam_group.developers.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowListActions",
+            "Effect": "Allow",
+            "Action": [
+                "iam:ListUsers",
+                "iam:ListVirtualMFADevices"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "BlockMostAccessUnlessSignedInWithMFA",
+            "Effect": "Deny",
+            "NotAction": [
+                "iam:CreateVirtualMFADevice",
+                "iam:EnableMFADevice",
+                "iam:ListMFADevices",
+                "iam:ListUsers",
+                "iam:ListVirtualMFADevices",
+                "iam:ResyncMFADevice"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "BoolIfExists": {
+                    "aws:MultiFactorAuthPresent": "false"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
