85fecda15f36d02a72797e0df61e4b88aa9daf43
Alberto del Barrio <adelbarrio@mozilla.com>
2020-07-02 17:32:46 +0200

    Remove IAM users in favor of SSO login via MAWS

--- README.md
+++ README.md
@@ -40,29 +40,17 @@ Run 'PODS=$(kubectl get po -n=discourse-prod -l=app=discourse | awk '/discourse/
 If you want a faster approach you can run `kubectl delete po -n=discourse-prod -l=app=discourse`. This will delete all the running pods and spawn new ones, because you are deleting all the pods at once, the application will experience a small downtime.
 
 # User management
-This section describes how to create new users (intented for developers) in AWS and how can they connect to the Kubernetes cluster.
+This section describes how to new users (intented for developers) can connect to the Kubernetes cluster.
 In order connect to the cluster, a user must have installed the next dependencies:
  - awscli >= 1.16.154
  - kubectl >= 1.13 
+ - maws
 
-### Creating a user in AWS
-AWS users are managed using Terraform. In order to create a new user, one has to edit the file `terraform/users.tf`. Copy the resources used by the user `lmcardle`, change the name of the new user and run `terraform apply` to apply the changes.
+They also have to be part of the `aws_discourse_dev` LDAP group.
 
-Once Terraform finishes, it should have created a set of credentials which will allow the user to visit the AWS Console. These credentials can be obtained running `terraform output password_username`, the output is a base64 blob containing the GPG encoded password. Now, give the credentials to the new user, she can decrypt the password running: `cat terraform_ouput_pw | base64 -d | keybase gpg decrypt`.
-
-#### Enrolling an MFA device
-The default user policy enforces a user to setup MFA, until it is enabled the user will not be able to see or interact without any AWS resource.
-In order to enable MFA for her account, the new user has to login into the AWS console, go to the IAM secction and follow the steps described in the UI to enroll her MFA device
-
-#### Generate API keys
-Now the user can generate a pair of API keys for using the command line interface. Inside the AWS console, go to IAM, select your user, click on "Security Credentials" and generate them.
-
-### Grant access to the Kubernetes cluster
-Granting access for a user to the Kubernetes cluster requires having 2 things: an AWS user with permissions to describe the target EKS cluster (this was done in the previous step), and a role inside Kubernetes granting permissions to certain namespaces. This role is already created for discourse developers, granting access to the 3 namespaces containing the different environments.
-
-Now the AWS user has to be mapped to the Kubernetes role. In order to perform the mapping, edit the `aws-auth` configmap and reflect the new mapping: `kubectl edit configmap aws-auth -n=kube-system`.
 
 ### Get EKS credentials and test access
+First run $(maws), a new browser window will open, there click on `maws-discourse-developers`. Now you will have the right IAM role.
 The configuration file which instructs kubectl how to connect to the cluster can be hand crafted, of obtained using the AWS cli. Obtaining it via the AWS cli is the easiest option and can be done running: `aws eks update-kubeconfig --name k8s-apps-prod-us-west-2`, this will create a file in `~/.kube/config`.
 
 It should be all set. Test the access running `kubectl get pods -n=discourse-dev`. If you see a list of pods, you are ready to go. 
--- terraform/users.tf
+++ terraform/users.tf
@@ -2,476 +2,81 @@
 #      Users       #
 ####################
 
-# bowlofstew
-resource "aws_iam_user" "bowlofstew" {
-  name  = "bowlofstew"
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  # Not supported by EKS:
-  #path = "/discourse/"
-  tags = var.common-tags
+// Grant access to users via MAWS
+module "discourse_developers" {
+  create_role  = terraform.workspace == "prod" ? "1" : "0"
+  source       = "github.com/mozilla-it/terraform-modules//aws/maws-roles?ref=7bc81e52cde07d213e9aaef5befe6bbe4de9764e"
+  role_name    = "maws-discourse-developers"
+  role_mapping = ["aws_discourse_dev"]
+  policy_arn   = terraform.workspace == "prod" ? aws_iam_policy.developers[0].arn : ""
 }
 
-resource "aws_iam_user_policy" "bowlofstew_mfa" {
-  name  = "allow-${aws_iam_user.bowlofstew[0].name}-self-manage-mfa"
-  user  = aws_iam_user.bowlofstew[0].name
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowIndividualUserToListOnlyTheirOwnMFA",
-      "Effect": "Allow",
-      "Action": "iam:ListMFADevices",
-      "Resource": [
-        "arn:aws:iam::*:mfa/*",
-        "arn:aws:iam::*:user/${aws_iam_user.bowlofstew[0].name}"
-      ]
-    },
-    {
-      "Sid": "AllowIndividualUserToManageTheirOwnMFA",
-      "Effect": "Allow",
-      "Action": [
-        "iam:CreateVirtualMFADevice",
-        "iam:DeleteVirtualMFADevice",
-        "iam:EnableMFADevice",
-        "iam:ResyncMFADevice"
-      ],
-      "Resource": [
-        "arn:aws:iam::*:mfa/${aws_iam_user.bowlofstew[0].name}",
-        "arn:aws:iam::*:user/${aws_iam_user.bowlofstew[0].name}"
-      ]
-    },
-    {
-      "Sid": "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA",
-      "Effect": "Allow",
-      "Action": "iam:DeactivateMFADevice",
-      "Resource": [
-        "arn:aws:iam::*:mfa/${aws_iam_user.bowlofstew[0].name}",
-        "arn:aws:iam::*:user/${aws_iam_user.bowlofstew[0].name}"
-      ],
-      "Condition": {
-         "Bool": {
-           "aws:MultiFactorAuthPresent": "true"
-          }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-				"iam:ChangePassword",
-        "iam:CreateAccessKey",
-        "iam:DeleteAccessKey",
-        "iam:GetAccessKeyLastUsed",
-        "iam:GetUser",
-        "iam:ListAccessKeys",
-        "iam:UpdateAccessKey"
-			],
-      "Resource": "arn:aws:iam::783633885093:user/${aws_iam_user.bowlofstew[0].id}"
-    }
-  ]
-}
-EOF
+resource "aws_iam_policy" "developers" {
+  count  = terraform.workspace == "prod" ? "1" : "0"
+  name   = "discourseDevelopersPolicy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.developers.json
 }
 
-resource "aws_iam_user_login_profile" "bowlofstew" {
-  user                    = aws_iam_user.bowlofstew[0].name
-  pgp_key                 = "keybase:stewart"
-  password_reset_required = false
-  count                   = terraform.workspace == "prod" ? "1" : "0"
+data "aws_iam_policy_document" "developers" {
 
-  lifecycle {
-    ignore_changes = [password_length, password_reset_required, pgp_key]
+  statement {
+    sid       = "DiscourseEks"
+    actions   = ["eks:DescribeCluster"]
+    resources = ["*"]
   }
-}
 
-# Alberto
-resource "aws_iam_user" "adelbarrio" {
-  name  = "adelbarrio"
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  # Not supported by EKS:
-  #path = "/discourse/"
-  tags = var.common-tags
-}
-
-resource "aws_iam_user_policy" "adelbarrio_mfa" {
-  name  = "allow-${aws_iam_user.adelbarrio[0].name}-self-manage-mfa"
-  user  = aws_iam_user.adelbarrio[0].name
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowIndividualUserToListOnlyTheirOwnMFA",
-      "Effect": "Allow",
-      "Action": "iam:ListMFADevices",
-      "Resource": [
-        "arn:aws:iam::*:mfa/*",
-        "arn:aws:iam::*:user/${aws_iam_user.adelbarrio[0].name}"
-      ]
-    },
-    {
-      "Sid": "AllowIndividualUserToManageTheirOwnMFA",
-      "Effect": "Allow",
-      "Action": [
-        "iam:CreateVirtualMFADevice",
-        "iam:DeleteVirtualMFADevice",
-        "iam:EnableMFADevice",
-        "iam:ResyncMFADevice"
-      ],
-      "Resource": [
-        "arn:aws:iam::*:mfa/${aws_iam_user.adelbarrio[0].name}",
-        "arn:aws:iam::*:user/${aws_iam_user.adelbarrio[0].name}"
-      ]
-    },
-    {
-      "Sid": "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA",
-      "Effect": "Allow",
-      "Action": "iam:DeactivateMFADevice",
-      "Resource": [
-        "arn:aws:iam::*:mfa/${aws_iam_user.adelbarrio[0].name}",
-        "arn:aws:iam::*:user/${aws_iam_user.adelbarrio[0].name}"
-      ],
-      "Condition": {
-         "Bool": {
-           "aws:MultiFactorAuthPresent": "true"
-          }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-				"iam:ChangePassword",
-        "iam:CreateAccessKey",
-        "iam:DeleteAccessKey",
-        "iam:GetAccessKeyLastUsed",
-        "iam:GetUser",
-        "iam:ListAccessKeys",
-        "iam:UpdateAccessKey"
-			],
-      "Resource": "arn:aws:iam::783633885093:user/${aws_iam_user.adelbarrio[0].id}"
-    }
-  ]
-}
-EOF
-
-}
-
-resource "aws_iam_user_login_profile" "adelbarrio" {
-  user                    = aws_iam_user.adelbarrio[0].name
-  pgp_key                 = "keybase:adelbarrio"
-  password_reset_required = false
-  count                   = terraform.workspace == "prod" ? "1" : "0"
-
-  lifecycle {
-    ignore_changes = [
-      password_length,
-      password_reset_required,
-      pgp_key,
+  statement {
+    sid     = "DiscourseCodebuild"
+    actions = ["codebuild:*"]
+    resources = [
+      "arn:aws:codebuild:us-west-2:783633885093:project/discourse-stage",
+      "arn:aws:codebuild:us-west-2:783633885093:project/discourse-dev",
+      "arn:aws:codebuild:us-west-2:783633885093:project/discourse-prod"
     ]
   }
-}
 
-# Leo
-resource "aws_iam_user" "lmcardle" {
-  name  = "lmcardle"
-  count = terraform.workspace == "prod" ? "1" : "0"
+  statement {
+    sid       = "DiscourseS3"
+    actions   = ["s3:*"]
+    resources = ["arn:aws:s3:::discourse-*"]
+  }
 
-  # Not supported by EKS:
-  #path = "/discourse/"
-  tags = var.common-tags
-}
-
-resource "aws_iam_user_policy" "lmcardle_mfa" {
-  name  = "allow-${aws_iam_user.lmcardle[0].name}-self-manage-mfa"
-  user  = aws_iam_user.lmcardle[0].name
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AllowIndividualUserToListOnlyTheirOwnMFA",
-      "Effect": "Allow",
-      "Action": "iam:ListMFADevices",
-      "Resource": [
-        "arn:aws:iam::*:mfa/*",
-        "arn:aws:iam::*:user/${aws_iam_user.lmcardle[0].name}"
-      ]
-    },
-    {
-      "Sid": "AllowIndividualUserToManageTheirOwnMFA",
-      "Effect": "Allow",
-      "Action": [
-        "iam:CreateVirtualMFADevice",
-        "iam:DeleteVirtualMFADevice",
-        "iam:EnableMFADevice",
-        "iam:ResyncMFADevice"
-      ],
-      "Resource": [
-        "arn:aws:iam::*:mfa/${aws_iam_user.lmcardle[0].name}",
-        "arn:aws:iam::*:user/${aws_iam_user.lmcardle[0].name}"
-      ]
-    },
-    {
-      "Sid": "AllowIndividualUserToDeactivateOnlyTheirOwnMFAOnlyWhenUsingMFA",
-      "Effect": "Allow",
-      "Action": "iam:DeactivateMFADevice",
-      "Resource": [
-        "arn:aws:iam::*:mfa/${aws_iam_user.lmcardle[0].name}",
-        "arn:aws:iam::*:user/${aws_iam_user.lmcardle[0].name}"
-      ],
-      "Condition": {
-         "Bool": {
-           "aws:MultiFactorAuthPresent": "true"
-          }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-				"iam:ChangePassword",
-        "iam:CreateAccessKey",
-        "iam:DeleteAccessKey",
-        "iam:GetAccessKeyLastUsed",
-        "iam:GetUser",
-        "iam:ListAccessKeys",
-        "iam:UpdateAccessKey"
-			],
-      "Resource": "arn:aws:iam::783633885093:user/${aws_iam_user.lmcardle[0].id}"
-    }
-  ]
-}
-EOF
-
-}
-
-resource "aws_iam_user_login_profile" "lmcardle" {
-  user                    = aws_iam_user.lmcardle[0].name
-  pgp_key                 = "keybase:leomca"
-  password_reset_required = false
-  count                   = terraform.workspace == "prod" ? "1" : "0"
-
-  lifecycle {
-    ignore_changes = [
-      password_length,
-      password_reset_required,
-      pgp_key,
+  statement {
+    sid = "DiscourseSES"
+    actions = [
+      "ses:DescribeActiveReceiptRuleSet",
+      "ses:DescribeReceiptRule",
+      "ses:DescribeReceiptRuleSet",
+      "ses:ListReceiptRuleSets"
     ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "DiscourseLambdaRO"
+    actions = [
+      "lambda:GetAccountSettings",
+      "lambda:ListFunctions",
+      "lambda:ListTags",
+      "lambda:GetEventSourceMapping",
+      "lambda:ListEventSourceMappings"
+    ]
+    resources = ["arn:aws:lambda:::discourse-*"]
+  }
+
+  statement {
+    sid = "DiscourseLambdaSourceMapping"
+    actions = [
+      "lambda:DeleteEventSourceMapping",
+      "lambda:UpdateEventSourceMapping",
+      "lambda:CreateEventSourceMapping"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "lambda:FunctionArn"
+      values   = ["arn:aws:lambda:*:*:function:discourse-*"]
+    }
   }
 }
-
-#####################
-#   Group policies  #
-#####################
-
-resource "aws_iam_group_membership" "discourse" {
-  name  = "discourse-developers"
-  count = terraform.workspace == "prod" ? "1" : "0"
-  users = [aws_iam_user.adelbarrio[0].name, aws_iam_user.lmcardle[0].name]
-  group = aws_iam_group.developers[0].name
-}
-
-resource "aws_iam_group" "developers" {
-  name  = "developers"
-  path  = "/discourse/"
-  count = terraform.workspace == "prod" ? "1" : "0"
-}
-
-resource "aws_iam_group_policy" "discourse-devs" {
-  name  = "discourse-developer-policy"
-  group = aws_iam_group.developers[0].id
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "eks:DescribeCluster",
-        "eks:ListClusters",
-				"codebuild:ListProjects"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": "iam:GetAccountPasswordPolicy",
-       "Resource": "*"
-    },
-    {
-      "Action": [
-        "codebuild:*"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-				"arn:aws:codebuild:us-west-2:783633885093:project/discourse-stage",
-				"arn:aws:codebuild:us-west-2:783633885093:project/discourse-dev",
-				"arn:aws:codebuild:us-west-2:783633885093:project/discourse-prod"
-			]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "events:DescribeRule",
-        "events:ListTargetsByRule",
-        "events:ListRuleNamesByTarget",
-				"cloudwatch:GetMetricStatistics",
-        "s3:GetBucketLocation",
-        "s3:ListAllMyBuckets",
-        "logs:GetLogEvents"
-			],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ssm:PutParameter"
-      ],
-      "Resource": "arn:aws:ssm:*:*:parameter/CodeBuild/*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ses:DescribeActiveReceiptRuleSet",
-        "ses:DescribeReceiptRule",
-        "ses:DescribeReceiptRuleSet",
-        "ses:ListReceiptRuleSets"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:*"
-      ],
-      "Resource": [
-				"arn:aws:s3:::discourse-*"
-
-			]
-    }
-  ]
-}
-EOF
-
-}
-
-resource "aws_iam_group_policy" "self-managed-mfa" {
-  name  = "self-managed-mfa"
-  group = aws_iam_group.developers[0].id
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowListActions",
-            "Effect": "Allow",
-            "Action": [
-                "iam:ListUsers",
-                "iam:ListVirtualMFADevices"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "BlockMostAccessUnlessSignedInWithMFA",
-            "Effect": "Deny",
-            "NotAction": [
-                "iam:CreateVirtualMFADevice",
-                "iam:EnableMFADevice",
-                "iam:ListMFADevices",
-                "iam:ListUsers",
-                "iam:ListVirtualMFADevices",
-                "iam:ResyncMFADevice"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "BoolIfExists": {
-                    "aws:MultiFactorAuthPresent": "false"
-                }
-            }
-        }
-    ]
-}
-EOF
-
-}
-
-resource "aws_iam_group_policy" "lambda" {
-  name  = "discourse-developers-lambda-access"
-  group = aws_iam_group.developers[0].id
-  count = terraform.workspace == "prod" ? "1" : "0"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-			{
-        "Sid": "LambdaReadOnlyPermissions",
-        "Effect": "Allow",
-        "Action": [
-            "lambda:GetAccountSettings",
-            "lambda:ListFunctions",
-            "lambda:ListTags",
-            "lambda:GetEventSourceMapping",
-            "lambda:ListEventSourceMappings",
-            "iam:ListRoles"
-        ],
-         "Resource": "*"
-        },
-        {
-          "Sid": "LambdaDevelopFunctions",
-          "Effect": "Allow",
-          "NotAction": [
-             "lambda:AddPermission",
-             "lambda:PutFunctionConcurrency"
-          ],
-          "Resource": "arn:aws:lambda:*:*:function:discourse-*"
-        },
-        {
-          "Sid": "DiscourseLambdaDevelopEventSourceMappings",
-           "Effect": "Allow",
-           "Action": [
-              "lambda:DeleteEventSourceMapping",
-              "lambda:UpdateEventSourceMapping",
-              "lambda:CreateEventSourceMapping"
-           ],
-           "Resource": "*",
-           "Condition": {
-              "StringLike": {
-                 "lambda:FunctionArn": "arn:aws:lambda:*:*:function:discourse-*"
-               }
-            }
-        },
-        {
-          "Sid": "DiscoursLambdaeViewLogs",
-          "Effect": "Allow",
-          "Action": [
-              "logs:*"
-          ],
-          "Resource": "arn:aws:logs:*:*:log-group:/aws/lambda/discourse-*"
-        },
-        {
-          "Sid": "DiscoursLambdaeViewMetrics",
-          "Effect": "Allow",
-          "Action": [
-              "cloudwatch:GetMetricStatistics",
-							"cloudwatch:GetMetricData"
-          ],
-          "Resource": "*"
-        }
-    ]
-}
-EOF
-
-}
-
